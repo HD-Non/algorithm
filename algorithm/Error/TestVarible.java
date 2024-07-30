076c9b9dbb04eea04542620b5f8be847183a6251
HD-Non <674259027@webmail.npru.ac.th>
2024-07-30 11:43:53 +0700

    first commit

---
+++ algorithm/Error/TestVarible.java
@@ -0,0 +1,24 @@
+   package Error;
+public class TestVarible {
+   public static void main (String[] args){
+    char clothingSize ='S';
+    System.out.println("clothing ="+clothingSize);
+    int websiteVistors = 0;
+    System.out.println("websiteVistors =" +websiteVistors);
+    char StudentGarde = 'A';
+    System.out.println("StudentGarde = "+StudentGarde);
+    float StudentGPA = 4.0F;
+    System.out.println("StudentGPA = "+StudentGPA);
+    final double PI = 3.14159;
+    System.out.println("Final double PI = "+PI);
+    int Studentannualexpenes = 300000;
+    System.out.println("Stuent annual ="+Studentannualexpenes);
+    short month = 30000;
+    System.out.println("short month ="+ month);
+   }  
+
+
+
+}
+
+
---
+++ algorithm/Error/syntax1.java
@@ -0,0 +1,8 @@
+package Error;
+public class syntax1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //ทดสอบ syntax error ลบ;
+        System.out.println(4/2); // ทดสอบ runtime error ทำส่วนเป็นศูนย์
+        System.out.println(4.0/100); // ทดสอบ runtime error ทำส่วนเป็นศูนย์
+    }
+}
